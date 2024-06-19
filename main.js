1f8801a3d5d04ada886683797a7e26f8adfb966a
aditya1101raj <159050070+aditya1101raj@users.noreply.github.com>
2024-06-19 16:09:12 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var typed = new Typed(".text",{
-    strings: ["Student .","Frontend Developer .","Musician .","Tech Enthusiast ."],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-})
-
